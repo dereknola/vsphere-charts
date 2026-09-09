d28d0b8ae13e55d26bb1409c19bd05d1bb0b9f95
Derek Nola <derek.nola@suse.com>
2026-09-09 15:39:21 -0700

    Test bad commit
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/unit/cpi_template_test.go
+++ tests/unit/cpi_template_test.go
@@ -45,7 +45,7 @@ func TestCPITemplateRenderedDaemonset(t *testing.T) {
 				namespace:     "cpitest",
 				releaseName:   "cpitest",
 				chartRelPath:  cpiChart,
-				expectedImage: "rancher/hardened-cloud-provider-vsphere:v1.37.0-build20260909",
+				expectedImage: "rancher/hardened-cloud-provider-vsphere:v1.37.0-build202609010",
 			},
 		},
 		{
